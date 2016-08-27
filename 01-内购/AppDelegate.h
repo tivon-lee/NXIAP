5dda18caa7099a0fe8af49ba0a0708bb7f247ad5
litelin <iamleetvin@gmail.com>
2016-08-27 10:33:26 +0800

    初始化

---
+++ 01-内购/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  01-内购
+//
+//  Created by apple on 16/8/26.
+//  Copyright © 2016年 macbook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 01-内购/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  01-内购
+//
+//  Created by apple on 16/8/26.
+//  Copyright © 2016年 macbook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UITableViewController
+
+
+@end
+
